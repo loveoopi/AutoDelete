c3219db481eafba81f5e0baef351a4a402eddca5
Mayank rohilla <158100710+Maya12981@users.noreply.github.com>
2025-05-10 10:25:47 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -18,13 +18,13 @@
 
 import os
 
-API_ID       = int(os.environ.get("API_ID", "22894706"))
-API_HASH     = os.environ.get("API_HASH", "13c8f765d49c935d2ffd9152f8430f7e")
+API_ID       = int(os.environ.get("API_ID", "20284828"))
+API_HASH     = os.environ.get("API_HASH", "a980ba25306901d5c9b899414d6a9ab7")
 BOT_TOKEN    = os.environ.get("BOT_TOKEN", "7148114178:AAGpKYE4hsBg7INn0LqPryseKvSrreP_ZSM")
-SESSION      = os.environ.get("SESSION", "BQC6kfsABhUpjLg4BUPOk-ot7uV_MX25n370c6mwHvUFp1GMAaJEwYHg_UEl4jTqGtGg0Zm_ijo4UzB8VMUVHCVSEW-6zrPXMu5sWE3tZpamh7isq0pKwdw8IMHpyGCjy35DimhKibNvkUt1Zy9bfABJPIuUhuyTqcP9aGmt3HAyJF3dj7UgdyLwjDcURuZ636UicPqKpYFj1wR2fqvIJwCsVcEkh0DQmYsQrhN_kthgde6OWU_4wponGLG2-6cftFwvFX_HziYSGhCK4YisiJcY6lOZcQ4jPxRD9ENsMHrEtst_lK7NkXCGdJ_I4ZHxcbvKebOMenENtJOBvaI4c-fG7GGCCwAAAAGnUoSaAA")
+SESSION      = os.environ.get("SESSION", "BQA8vjJaOVSYIzSY6LBopOnWCB7_mwt9zyTlOzAe1hw7icdAJhi2N7V189d3iJLB5b_9xHec2mNWSaoWloVNXIQ_t0A55v_Cq41DhlHB39JFHdKecaYQlDazTXl_lCC6LCYImJtaozMvUObjCFd6E4Yi5WNGOkmmYdfxFFFmVD-5Q01AuJSYKI5n8umh4LCujXOqR9dbTOmjl-4XqI4hGi08Pvd4ogcyIUFgka4UWNQtpWfvVx_wQnols5FMNL-qH_q4Ck5qpVR2EpBZO4J8MbKkppBjo-yWbZ9jZy6Xb3BNcLTslJlBO75P-SwaUnpSK0Mwq8cN_ckehk_yikWc67JQAAAAAbgp_n0A")
 TIME         = int(os.environ.get("TIME", 10))
 CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
 BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Kingrr:4qGUNmnLghYtCQRc@cluster0.s5ezh5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://yacan69355:Cw92BrnfAfWQcLvU@cluster0.jh6h6wg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 PORT         = os.environ.get("PORT", "8080")
